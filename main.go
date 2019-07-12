949e1500de002aba297cffb35ed5913af3b49bfb
Manuel <thetipo012001@gmail.com>
2019-07-12 21:30:25 +0200

    Now the program monitors for clipboard changes!

--- main.go
+++ main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"encoding/base64"
+	"fmt"
 	"github.com/atotto/clipboard"
+	"log"
+	"net/url"
 	"os/exec"
 	"runtime"
+	"time"
 )
 
-func main() {
-	url, _ := clipboard.ReadAll()
-	decoded, _ := base64.StdEncoding.DecodeString(url)
-	openbrowser(string(decoded))
-	_ = clipboard.WriteAll(string(decoded))
+func isValidUrl(toTest string) bool {
+	_, err := url.ParseRequestURI(toTest)
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
 }
 
 func openbrowser(url string) {
@@ -24,4 +30,19 @@ func openbrowser(url string) {
 	case "darwin":
 		_ = exec.Command("open", url).Start()
 	}
+}
+
+func main() {
+	fmt.Println("Running. To stop press CTRL+C")
+	for {
+		clip, _ := clipboard.ReadAll()
+		decoded, _ := base64.StdEncoding.DecodeString(clip)
+		if isValidUrl(string(decoded)) {
+			log.Print("URL detected")
+			openbrowser(string(decoded))
+			_ = clipboard.WriteAll(string(decoded))
+		}
+		time.Sleep(time.Second / 2)
+	}
+
 }
