de49c9ecd130a82ca05051b97f632c5d694d83f7
Manuel <10187614+TheTipo01@users.noreply.github.com>
2019-07-12 21:31:44 +0200

    Revert "Now the program monitors for clipboard changes!"
    This reverts commit 949e1500de002aba297cffb35ed5913af3b49bfb.

--- main.go
+++ main.go
@@ -2,22 +2,16 @@ package main
 
 import (
 	"encoding/base64"
-	"fmt"
 	"github.com/atotto/clipboard"
-	"log"
-	"net/url"
 	"os/exec"
 	"runtime"
-	"time"
 )
 
-func isValidUrl(toTest string) bool {
-	_, err := url.ParseRequestURI(toTest)
-	if err != nil {
-		return false
-	} else {
-		return true
-	}
+func main() {
+	url, _ := clipboard.ReadAll()
+	decoded, _ := base64.StdEncoding.DecodeString(url)
+	openbrowser(string(decoded))
+	_ = clipboard.WriteAll(string(decoded))
 }
 
 func openbrowser(url string) {
@@ -30,19 +24,4 @@ func openbrowser(url string) {
 	case "darwin":
 		_ = exec.Command("open", url).Start()
 	}
-}
-
-func main() {
-	fmt.Println("Running. To stop press CTRL+C")
-	for {
-		clip, _ := clipboard.ReadAll()
-		decoded, _ := base64.StdEncoding.DecodeString(clip)
-		if isValidUrl(string(decoded)) {
-			log.Print("URL detected")
-			openbrowser(string(decoded))
-			_ = clipboard.WriteAll(string(decoded))
-		}
-		time.Sleep(time.Second / 2)
-	}
-
 }
